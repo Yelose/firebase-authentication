ba2f69661d81c7258af1b8f3cff876f0c91efc96
Marina González Suárez <yelose85@gmail.com>
2022-11-08 13:56:04 +0100

    changed firebase .json to add rule

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,36 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import { initializeApp } from 'firebase/app';
 
-export const environment = {
-  firebase: {
-    projectId: 'yelose-meetme',
-    appId: '1:193295748636:web:8865395de7fce2dfaa19d9',
-    storageBucket: 'yelose-meetme.appspot.com',
-    apiKey: 'AIzaSyC7HJmtrb5wfForynq0XL8OO1wRfk_Ve7I',
-    authDomain: 'yelose-meetme.firebaseapp.com',
-    messagingSenderId: '193295748636',
-    measurementId: 'G-3RB0ZJTTEW',
-  },
-  production: false
+import { getAnalytics } from 'firebase/analytics';
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC7HJmtrb5wfForynq0XL8OO1wRfk_Ve7I',
+
+  authDomain: 'yelose-meetme.firebaseapp.com',
+
+  projectId: 'yelose-meetme',
+
+  storageBucket: 'yelose-meetme.appspot.com',
+
+  messagingSenderId: '193295748636',
+
+  appId: '1:193295748636:web:a987dbe15c4ac3ceaa19d9',
+
+  measurementId: 'G-K31QBKSD51',
 };
 
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
